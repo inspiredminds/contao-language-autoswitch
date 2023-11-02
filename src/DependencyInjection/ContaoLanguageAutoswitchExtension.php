47a7bbee4c0478a1c82feeb60e79cc3da38b9c7c
fritzmg <fmg@inspiredminds.at>
2023-11-02 15:55:29 +0000

    add support for category target pages

---
+++ src/DependencyInjection/ContaoLanguageAutoswitchExtension.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) INSPIRED MINDS
+ */
+
+namespace InspiredMinds\ContaoLanguageAutoswitch\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ContaoLanguageAutoswitchExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        (new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config')))
+            ->load('services.yaml')
+        ;
+    }
+}
---
+++ src/NewsCategoriesManager.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * (c) INSPIRED MINDS
+ */
+
+namespace InspiredMinds\ContaoLanguageAutoswitch;
+
+use Codefog\NewsCategoriesBundle\Model\NewsCategoryModel;
+use Codefog\NewsCategoriesBundle\NewsCategoriesManager as NewsCategoriesBundleNewsCategoriesManager;
+use Contao\CoreBundle\Framework\ContaoFramework;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Terminal42\ChangeLanguage\PageFinder;
+
+class NewsCategoriesManager extends NewsCategoriesBundleNewsCategoriesManager
+{
+    public function __construct(
+        private readonly PageFinder $pageFinder,
+        private readonly RequestStack $requestStack,
+        protected ContaoFramework|null $framework,
+    ) {
+    }
+
+    public function getTargetPage(NewsCategoryModel $category)
+    {
+        if (!$targetPage = parent::getTargetPage($category)) {
+            return $targetPage;
+        }
+
+        $request = $this->requestStack->getCurrentRequest();
+
+        if ($request && ($otherPage = $this->pageFinder->findAssociatedForLanguage($targetPage, $request->getLocale()))) {
+            return $otherPage;
+        }
+
+        return $targetPage;
+    }
+}
