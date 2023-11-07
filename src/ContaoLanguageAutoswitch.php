c80523e32ae912f06581fedfc383b818d8b1572e
fritzmg <fmg@inspiredminds.at>
2023-11-07 09:43:26 +0000

    fix some compatibility issues

--- src/ContaoLanguageAutoswitch.php
+++ src/ContaoLanguageAutoswitch.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ContaoLanguageAutoswitch extends Bundle
 {
-    public function getPath()
+    public function getPath(): string
     {
         return \dirname(__DIR__);
     }
